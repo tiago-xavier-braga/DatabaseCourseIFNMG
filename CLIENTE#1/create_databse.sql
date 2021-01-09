9a467950ce71272e69ffb8c1e7281b8bc6fffae1
tiago-xavier-braga <braga.tiagoxavier@gmail.com>
2021-01-08 21:35:58 -0300

    commit#4
    08/01/2020

--- CLIENTE#1/create_databse.sql
+++ CLIENTE#1/create_databse.sql
@@ -7,7 +7,7 @@ CREATE TABLE CLIENTE(
     NOME VARCHAR(30),
     SEXO CHAR(1),
     EMAIL VARCHAR(30),
-    CPF INT(11),
+    CPF CHAR(14),
     TELEFONE VARCHAR(30),
     ENDERECO VARCHAR(100)
 );
@@ -15,3 +15,20 @@ CREATE TABLE CLIENTE(
 SHOW TABLES;
 /*Mostrando os campos da tabela*/
 DESC CLIENTE;
+/*Removendo tabelas*/
+DROP TABLE CLIENTE;
+/*Colocando dados*/
+INSERT INTO CLIENTE(NOME, SEXO, EMAIL, CPF, TELEFONE, ENDERECO) VALUES (
+    'Carlos Ducati Teixeira', 
+    'M', 
+    'carlos.teixeira@gmail.com', 
+    '825.511.442-48',
+    '(68) 98140-5027',
+    'Travessa Ano Novo - Paz - Rio Branco');
+INSERT INTO CLIENTE(NOME, SEXO, EMAIL, CPF, TELEFONE, ENDERECO) VALUES (
+    'Laura Gomes Amaral',
+    'F',
+    'laura.amaral@gmail.com',
+    '761.311.857-60',
+    '(82) 97922-0541',
+    'Antenor Gomes de Oliveira - Farol - Maceió');
